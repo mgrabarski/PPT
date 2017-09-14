afde94dfd41f5e47cd2ba454f138eb5e38513c97
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2017-09-14 19:26:03 +0200

    added pojos for scores and xml handler

---
+++ businesslogiclayer/src/main/java/mateusz/grabarski/businesslogiclayer/models/scores/Parameter.java
@@ -0,0 +1,63 @@
+package mateusz.grabarski.businesslogiclayer.models.scores;
+
+/**
+ * Created by Mateusz Grabarski on 14.09.2017.
+ */
+
+public class Parameter {
+
+    private String name;
+    private String value;
+
+    public Parameter() {
+    }
+
+    public Parameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Parameter)) return false;
+
+        Parameter parameter = (Parameter) o;
+
+        if (getName() != null ? !getName().equals(parameter.getName()) : parameter.getName() != null)
+            return false;
+        return getValue() != null ? getValue().equals(parameter.getValue()) : parameter.getValue() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Parameter{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
